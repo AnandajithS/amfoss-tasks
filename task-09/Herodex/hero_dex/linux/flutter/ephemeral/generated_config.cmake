98b594cbfe27a596744f37871ac70a33651d99e1
AnandajithS <anandajiths2006@gmail.com>
2024-10-12 20:13:32 +0530

    pushing nano

---
+++ task-09/Herodex/hero_dex/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/anandajith-s/amfoss-tasks/task-09/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/anandajith-s/amfoss-tasks/task-09/Herodex/hero_dex" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/anandajith-s/amfoss-tasks/task-09/flutter"
+  "PROJECT_DIR=/home/anandajith-s/amfoss-tasks/task-09/Herodex/hero_dex"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/anandajith-s/amfoss-tasks/task-09/Herodex/hero_dex/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/anandajith-s/amfoss-tasks/task-09/Herodex/hero_dex/lib/main.dart"
+)
